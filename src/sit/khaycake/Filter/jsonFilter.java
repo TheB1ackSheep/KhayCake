6fecd35135fe0d26d1e0de12c5024092a4ff688f
Pasuth <pasuth73@hotmail.com>
2015-04-22 21:11:59 +0700

    set filter json

--- src/sit/khaycake/Filter/jsonFilter.java
+++ src/sit/khaycake/Filter/jsonFilter.java
@@ -1,7 +1,28 @@
 package sit.khaycake.Filter;
 
+import javax.servlet.*;
+import java.io.IOException;
+
 /**
  * Created by Pasuth on 22/4/2558.
  */
-public class jsonFilter {
+public class jsonFilter implements Filter {
+    FilterConfig config;
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        config = filterConfig;
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        servletRequest.setCharacterEncoding("UTF-8");
+        filterChain.doFilter(servletRequest,servletResponse);
+        servletResponse.setContentType("application/json");
+        servletResponse.setCharacterEncoding("UTF-8");
+    }
+
+    @Override
+    public void destroy() {
+
+    }
 }
